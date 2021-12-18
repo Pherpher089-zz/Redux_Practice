0c1d3c59a822d720d23794808278c51858c530f1
Chris <christutor089@gmail.com>
2021-12-18 02:44:40 -0800

    created app and began setting up redux

---
+++ redux-practice/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider, createStore } from 'react-redux'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
